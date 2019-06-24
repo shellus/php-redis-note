67883d24d86e02b736b697df189610af84385512
shellus <shellus@endaosi.com>
2019-06-24 16:20:58 +0800

    更新容器库

--- public/index.php
+++ public/index.php
@@ -1,26 +1,29 @@
 <?php
+
 use Symfony\Component\HttpFoundation\Response as BaseResponse;
+
 header("Content-Type:text/html;charset=UTF-8");
 
 require_once '../vendor/autoload.php';
 
-define('APP_PATH', __DIR__.'/..');
+define('APP_PATH', __DIR__ . '/..');
 
-$container = new \Orno\Di\Container;
+$container = new \League\Container\Container();
 
 require_once '../app/providers.php';
 
 function app($class_name)
 {
     global $container;
-    return $container -> get($class_name);
+    return $container->get($class_name);
 }
 
 /**
  * @param $view
  * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
  */
-function view($view){
+function view($view)
+{
     $response = BinaryFileResponse::create(APP_PATH . "/app/view/{$view}.html");
     return $response;
 }
@@ -29,12 +32,12 @@ $router = new Router();
 
 require_once '../app/routes.php';
 
-$router -> run(function($response){
-    if($response instanceof BaseResponse){
+$router->run(function ($response) {
+    if ($response instanceof BaseResponse) {
         $request = app('request');
         $response->prepare($request);
-        $response -> send();
-    }else{
+        $response->send();
+    } else {
         throw new Exception('不能识别的响应');
     }
 });
